cb3ff13c63b63762534af1db8fc4330c72fcff59
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-11-14 22:55:53 +0100

    add new fileds to book and author

--- src/app/catalog/book.ts
+++ src/app/catalog/book.ts
@@ -7,38 +7,24 @@ export class Book {
   private _year: number;
   private _price: number;
   private _coverUrl: string;
+  private _bookCoverId: string;
   private _available: number;
-  private _authors: Author[];
+  private _linkedAuthors: Author[];
 
   public get id(): number {
     return this._id;
-  }
-  public set id(value: number) {
-    this._id = value;
   }
 
   public get title(): string {
     return this._title;
   }
 
-  public set title(value: string) {
-    this._title = value;
-  }
-
   public get year(): number {
     return this._year;
   }
 
-  public set year(value: number) {
-    this._year = value;
-  }
-
   public get price(): number {
     return this._price;
-  }
-
-  public set price(value: number) {
-    this._price = value;
   }
 
   public get coverUrl(): string {
@@ -52,11 +38,13 @@ export class Book {
   public get available(): number {
     return this._available;
   }
-  public set available(value: number) {
-    this._available = value;
+
+  public get linkedAuthors(): Author[] {
+    return this._linkedAuthors;
   }
 
-
-
+  public get bookCoverId(): string {
+    return this._bookCoverId;
+  }
 }
 
